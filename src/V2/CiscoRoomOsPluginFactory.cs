4c6fa86c134fa66751121dc603ac32246bc81f59
Nick Genovese <ngenovese@pepperdash.com>
2024-02-17 18:35:51 -0800

    feat: wip for v2

--- src/V2/CiscoRoomOsPluginFactory.cs
+++ src/V2/CiscoRoomOsPluginFactory.cs
@@ -8,12 +8,14 @@ namespace epi_videoCodec_ciscoExtended.V2
     {
         public CiscoRoomOsPluginFactory()
         {
-            TypeNames = new List<string> {"ciscoRoomOs"};
+            TypeNames = new List<string> {"ciscoRoomOsV2"};
         }
 
         public override EssentialsDevice BuildDevice(DeviceConfig dc)
         {
-            return new CiscoRoomOsDevice(dc);
+            var props = dc.Properties.ToObject<CiscoCodecConfig>();
+            var communications = CommFactory.CreateCommForDevice(dc);
+            return new CiscoRoomOsDevice(dc.Key, dc.Name, props, communications);
         }
     }
 }
--- src/V2/FeedbackDebugExt.cs
+++ src/V2/FeedbackDebugExt.cs
@@ -10,13 +10,13 @@ namespace epi_videoCodec_ciscoExtended.V2
             feedback.OutputChange += (sender, args) =>
             {
                 if (sender is BoolFeedback)
-                    Debug.Console(0, parent, "Received {0} Update : '{1}'", feedback.Key, args.BoolValue);
+                    Debug.Console(1, parent, "Received {0} Update : '{1}'", feedback.Key, args.BoolValue);
 
                 if (sender is IntFeedback)
-                    Debug.Console(0, parent, "Received {0} Update : '{1}'", feedback.Key, args.IntValue);
+                    Debug.Console(1, parent, "Received {0} Update : '{1}'", feedback.Key, args.IntValue);
 
                 if (sender is StringFeedback)
-                    Debug.Console(0, parent, "Received {0} Update : '{1}'", feedback.Key, args.StringValue);
+                    Debug.Console(1, parent, "Received {0} Update : '{1}'", feedback.Key, args.StringValue);
             };
         }
     }
