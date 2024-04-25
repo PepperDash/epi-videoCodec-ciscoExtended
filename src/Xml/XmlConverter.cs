8126cb6f57c42f9791be2187119ed262c419e142
Joshua_Gutenplan <18546589+jgutenplan@users.noreply.github.com>
2024-04-24 21:22:23 -0700

    feat: xmlConverter

---
+++ src/Xml/XmlConverter.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace epi_videoCodec_ciscoExtended.Xml
+{
+    public class XmlConverter
+    {
+        public static string SerializeObject<T>(T toSerialize)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(T));
+            using (StringWriter textWriter = new StringWriter())
+            {
+                xmlSerializer.Serialize(textWriter, toSerialize);
+                return textWriter.ToString();
+            }
+        }
+    }
+}
