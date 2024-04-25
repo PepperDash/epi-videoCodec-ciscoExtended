4feb4690df7cbbdf2fdd4e3af01e9e89e86cc55c
Joshua_Gutenplan <18546589+jgutenplan@users.noreply.github.com>
2024-04-24 21:27:08 -0700

    feat: UserInterfaceExtensions

---
+++ src/UserInterfaceExtensions/Extensions.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using epi_videoCodec_ciscoExtended.UserInterfaceExtensions.Panels;
+using epi_videoCodec_ciscoExtended.Xml;
+using PepperDash.Core;
+using Serilog.Events;
+using Crestron.SimplSharp;
+using System;
+using epi_videoCodec_ciscoExtended.UserInterfaceExtensions;
+using epi_videoCodec_ciscoExtended.UserInterfaceWebViewDisplay;
+
+namespace epi_videoCodec_ciscoExtended.UserInterfaceExtensions
+{
+	/// <summary>
+	/// json config to build xml commands
+	/// xml config for command raw data to configure UIExtensions via API
+	/// Json attributes for config, xml attributes for command structure
+	/// </summary>
+	[XmlRoot("Extensions")]
+	public class Extensions: ICiscoCodecUiExtensions
+	{
+		[XmlElement("Version")]
+		public string Version { get; set; }
+
+		[JsonProperty("configId")] //0-40
+		public ushort ConfigId { get; set; }
+
+		[XmlElement("Panel")]
+		[JsonProperty("panel")]
+		public List<Panel> Panels { get; set; }
+		//other extensions later
+
+		public PanelsHandler PanelsHandler { get; set; }
+
+		public void Initialize(IKeyed parent, IBasicCommunication coms)
+		{
+			PanelsHandler = new PanelsHandler(parent, coms, Panels);
+			coms.SendText(xCommand());
+		}
+		
+		/// <summary>
+		/// string literal for multiline command
+		/// </summary>
+		/// <returns></returns>
+		public string xCommand() => $@"xCommand UserInterface Extensions Set ConfigId: {ConfigId}
+{toXmlString()}
+.{CiscoCodec.Delimiter}";
+
+		/// <summary>
+		/// converts the props on this object with xml attributes to 
+		/// an xml string for the xCommand
+		/// </summary>
+		/// <returns></returns>
+		string toXmlString()
+		{
+			try
+			{
+				return XmlConverter.SerializeObject(this);
+			}
+			catch (System.Exception ex)
+			{
+				Debug.LogMessage(ex, "XML Command Serialize Failed", null);
+				return "";
+			}
+		}
+	}
+}
---
+++ src/UserInterfaceExtensions/UiExtensionsHandler.cs
@@ -0,0 +1,44 @@
+﻿using epi_videoCodec_ciscoExtended.UserInterfaceWebViewDisplay;
+using PepperDash.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace epi_videoCodec_ciscoExtended.UserInterfaceExtensions
+{
+	public class UiExtensionsHandler : ICiscoCodecUiExtensionsHandler, IVideoCodecUiExtensionsHandler
+	{
+		private readonly IKeyed _parent;
+		private readonly IBasicCommunication _coms;
+
+		public Action<UiWebViewDisplayActionArgs> UiWebViewDisplayAction { get; set; }
+
+		public event EventHandler<UiExtensionsPanelEventArgs> UiExtensionsPanelClickEvent;
+
+		public UiExtensionsHandler(IKeyed parent, IBasicCommunication coms)
+
+		{
+			_parent = parent;
+			_coms = coms;
+
+			//set the action that will run when called with args from elsewhere via interface
+			UiWebViewDisplayAction =
+				new Action<UiWebViewDisplayActionArgs>((UiWebViewDisplayActionArgs args) =>
+				{
+					UiWebViewDisplay uwvd = new UiWebViewDisplay { Header = args.Header, Url = args.Url, Mode = args.Mode, Title = args.Title };
+					coms.SendText(uwvd.xCommand());
+				});
+
+		}
+
+		public void ParseStatus(Panels.CiscoCodecEvents.Panel panel)
+		{
+			if (panel.Clicked != null && panel.Clicked.PanelId != null && panel.Clicked.PanelId.Value != null)
+			{
+				UiExtensionsPanelClickEvent?.Invoke(this, new UiExtensionsPanelEventArgs(true, panel.Clicked.PanelId.Value));
+			}
+		}
+	}
+}
